4e83a0fcceb4ebaec6050f10cf7dd6b7e9e6aae1
c00kamunga <tsothalanti2@yahoo.com>
2020-05-08 19:38:25 -0700

    created private route component

--- friends/src/App.js
+++ friends/src/App.js
@@ -5,7 +5,17 @@ import { Route } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      
+      <ul>
+        <li>
+          <Link to="/public">Public Page</Link>
+        </li>
+        <li>
+          <Link to ="/protected">Protected Page</Link>
+        </li>
+          <Route path="/public" component={public}/>
+          <Route path="/login" component={Login}/>
+          <PrivateRoute path="/protected" component={Protected}/>
+      </ul>
     </div>
   );
 }
