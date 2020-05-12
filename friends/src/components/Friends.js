bc39b49b4f64c4333ea01463a69994adf9bd7ada
c00kamunga <tsothalanti2@yahoo.com>
2020-05-11 17:08:50 -0700

    mapped over the data returned from the API and displayed the data to the view

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -5,22 +5,29 @@ const Friends = (props) => {
   const [friend, setFriend] = useState([]);
 
   useEffect(() => {
-      axiosWithAuth()
-        .get("/friends")
-        .then((res) => {
-          console.log(res);
-          setFriend(res);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    
-}, []);
+    axiosWithAuth()
+      .get("/friends")
+      .then((res) => {
+        console.log(res.data);
+        setFriend(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
+      
     <div>
-      <h1>Friends List</h1>
+      {friend.map(homies => (
+          <>
+        <h1>Name: {homies.name}</h1>
+        <h3>age: {homies.age}</h3>
+        <h3>email: {homies.email}</h3>
+        </>
+      ))}
     </div>
+    
   );
 };
 
